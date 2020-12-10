26db099a3c5a602c75b8b0b6c16c4db25191cceb
wwsmj0509 <wwsmj0509@naver.com>
2020-12-10 17:47:09 +0900

    minji

--- sample_login/src/com/board/service/imgDeleteService.java
+++ sample_login/src/com/board/service/imgDeleteService.java
@@ -2,14 +2,9 @@ package com.board.service;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-
-import org.apache.ibatis.session.SqlSession;
 
 import com.board.dao.imgBoard_dao;
-import com.board.entity.imgBoard_entity;
-import com.login.entity.login_entity;
-import com.reply.dao.imgReply_dao;
+
 
 import controller.CommandAction;
 
@@ -18,20 +13,19 @@ public class imgDeleteService implements CommandAction{
 	@Override
 	public String requestPro_action(HttpServletRequest request, HttpServletResponse response) throws Throwable {
 		request.setCharacterEncoding("UTF-8");
+		
 		System.out.println("delete enter");
 		
-		int boardIdx = (Integer)request.getAttribute("idx");
+		int seq = Integer.parseInt(request.getParameter("seq"));
+		int pg = Integer.parseInt(request.getParameter("pg"));
+				
+		imgBoard_dao dao = new imgBoard_dao();
+		dao.boardDelete(seq);
+				
+		request.setAttribute("pg", pg);
+				
+		return "board/delete.jsp";
 		
-		imgBoard_dao dao= new imgBoard_dao();
-		int n = 0;
-//		dao.boardDelte(boardIdx);
-		
-		if(n > 0) {
-			request.setAttribute("deleteOK", n);
-			return "boardList.do";
-		}else {
-			return "boardView.do?idx="+boardIdx;
-		}
 	}
 
 }
